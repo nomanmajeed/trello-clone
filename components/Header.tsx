f21875e50b6fcca4029ebdaaeaa592500b38720a
nomanmajeed <nomanmajeed986@gmail.com>
2023-08-30 18:00:06 +0500

    feat: background gradient color added

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,6 +8,11 @@ function Header() {
   return (
     <header>
         <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+            
+            {/* Hidden div for gradient background */}
+            <div className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 to-[#0055D1]
+            rounded-md filter blur-3xl opacity-50 -z-50" />
+            
             <Image
                 src="https://links.papareact.com/c2cdd5"
                 alt="Trello Logo"
@@ -29,8 +34,15 @@ function Header() {
                     </button>
                 </form>
 
-                <Avatar name="Noman Majeed" round size="50" />
+                <Avatar name="Noman Majeed" round size="50" color="#0055D1" />
             </div>
+        </div>
+
+        <div className="flex items-center justify-center px-5 py-5 md:py-5">
+            <p className="flex items-center p-5 text-sm font-light shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
+                <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1"/>
+                GPT is summarising your tasks for the day...
+            </p>
         </div>
         
     </header>
