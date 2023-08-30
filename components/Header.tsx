1d428735342e1f325e4b3ebe25f08263ebdfa870
nomanmajeed <nomanmajeed986@gmail.com>
2023-08-30 17:38:38 +0500

    feat: header section icon and search bar design done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+"use client"
+
+import Image from 'next/image'
+import { MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+import Avatar from 'react-avatar'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+        <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+            <Image
+                src="https://links.papareact.com/c2cdd5"
+                alt="Trello Logo"
+                width={300}
+                height={100}
+                className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
+            />
+
+            <div className="flex items-center space-x-5 flex-1 justify-end">
+                <form action="" className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
+                    <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+                    <input 
+                        type="text" 
+                        placeholder="Search"
+                        className="flex-1 outline-none p-2" 
+                    />
+                    <button type="submit" hidden>
+                        Search
+                    </button>
+                </form>
+
+                <Avatar name="Noman Majeed" round size="50" />
+            </div>
+        </div>
+        
+    </header>
   )
 }
 
