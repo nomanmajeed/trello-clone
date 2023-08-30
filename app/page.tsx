cd7df20a40a2d140b4d2a68f9007bd937e2db739
nomanmajeed <nomanmajeed986@gmail.com>
2023-08-30 16:50:46 +0500

    init: trello next app initialized

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/Header'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      Trell 2.0 AI Clone
+    </main>
+  )
+}
