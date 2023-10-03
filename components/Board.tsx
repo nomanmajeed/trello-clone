1898a57250544eba9728331bb50becca6a5041a7
nomanmajeed <nomanmajeed986@gmail.com>
2023-10-04 00:00:16 +0500

    feat: cross column drag n drop done

--- components/Board.tsx
+++ components/Board.tsx
@@ -7,7 +7,7 @@ import Column from "./Column";
 
 export default function Board() {
 
-  const [ board, getBoard, setBoardState ] = useBoardStore((state) => [state.board, state.getBoard, state.setBoardState])
+  const [ board, getBoard, setBoardState, updateTodoInDB ] = useBoardStore((state) => [state.board, state.getBoard, state.setBoardState, state.updateTodoInDB])
 
   useEffect(() => {
     getBoard()
@@ -28,6 +28,62 @@ export default function Board() {
         columns: rearrangedColumns
       })
     }
+
+    // This step is needed as the indexes are stored as numbers 0,1,2 etc insted of ids in DND lib.
+    const columns = Array.from(board.columns)
+    const startColIndex = columns[Number(source.droppableId)]
+    const endColIndex = columns[Number(destination.droppableId)]
+    
+    const startCol: Column = {
+      id: startColIndex[0],
+      todos: startColIndex[1].todos
+    }
+
+    const endCol: Column = {
+      id: endColIndex[0],
+      todos: endColIndex[1].todos
+    }
+
+    if (!startCol || !endCol) return;
+
+    if (source.index === destination.index && startCol === endCol) return;
+
+    const newTodos = startCol.todos
+    const [todoMoved] = newTodos.splice(source.index, 1)
+
+    if (startCol.id === endCol.id){
+      // Same Column task drag
+      newTodos.splice(destination.index, 0, todoMoved)
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos
+      }
+      const newColumns = new Map(board.columns)
+
+      setBoardState({...board, columns: newColumns})
+    } else{
+      // different column task drag 
+
+      const endTodos = Array.from(endCol.todos)
+      endTodos.splice(destination.index, 0, todoMoved)
+
+      const newColumns = new Map(board.columns)
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos
+      }
+
+      newColumns.set(startCol.id, newCol)
+      newColumns.set(endCol.id, {
+        id: endCol.id,
+        todos: endTodos
+      })
+
+      // Update state and DB
+      updateTodoInDB(todoMoved, endCol.id)
+      setBoardState({ ...board, columns: newColumns})
+    }
+
   }
 
   return (
